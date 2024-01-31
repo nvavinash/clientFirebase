bfce614106fd0bb5c04290d2bb28413b07eccf43
nvavinash <avinashmk42@gmail.com>
2024-01-31 13:25:51 +0530

    footer fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,8 @@ export const Footer =()=>{
 return(
     <>
     <div className="footer">
-    <h2> &copy; 2024 CodeViq.com All Rights Reserved.</h2>
+    <p>❤ </p>
+    <h2>With CodeViq.com &copy;Avinash All Rights Reserved.</h2>
     </div>
     </>
 )
